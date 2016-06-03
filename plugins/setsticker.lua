2e124a46e0d035164421141d50da30d94e7e0ad7
teledead <tele.dead@gmail.com>
2016-06-04 03:07:54 +0430

    Add files via upload

--- plugins/setsticker.lua
+++ plugins/setsticker.lua
@@ -1,14 +1,12 @@
-do
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = 'sticker.webp'
+    local file = '/root/pika/data/pika.webp'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
     send_document(get_receiver(msg), file, ok_cb, false)
- send_large_msg(receiver, 'Done!', ok_cb, false)
-    redis:del("photo:setsticker")
+    redis:del("photo:sticker")
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
@@ -18,27 +16,21 @@ local function run(msg,matches)
     local receiver = get_receiver(msg)
     local group = msg.to.id
     if msg.media then
-       if msg.media.type == 'photo' and redis:get("photo:setsticker") then
-        if redis:get("photo:setsticker") == 'waiting' then
+       if msg.media.type == 'photo' and redis:get("photo:sticker") then
+        if redis:get("photo:sticker") == 'waiting' then
           load_photo(msg.id, tosticker, msg)
         end
        end
     end
-   if matches[1] == "setsticker" and is_sudo(msg)then
-     redis:set("photo:setsticker", "waiting")
+    if matches[1] == "setsticker" and is_sudo(msg) then
+     redis:set("photo:sticker", "waiting")
      return 'Please send your photo now'
     end
- if matches[1]:lower() == 'spherobot' then
- send_document(get_receiver(msg), "sticker.webp", ok_cb, false)
-end
 end
 return {
   patterns = {
  "^[!#/](setsticker)$",
- "^([Ss][Pp][Hh][Ee][Rr][Oo]bot)$",
- "^[!#/]([Ss]pherobot)$",
  "%[(photo)%]",
   },
   run = run,
 }
-end
